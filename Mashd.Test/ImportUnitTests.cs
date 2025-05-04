8210e0db96cb57dd3602198a0fb7485861ec22c7
Christian Veis Morsing <cmorsi23@student.aau.dk>
2025-05-05 01:31:47 +0200

    feature: ImportUnitTests added

--- Mashd.Test/ImportUnitTests.cs
+++ Mashd.Test/ImportUnitTests.cs
@@ -1,20 +1,203 @@
 using Mashd.Backend;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
 
 namespace TestProject1;
 
 public class ImportUnitTests
 {
+    private static IParseTree ParseProgram(string input)
+    {
+        var inputStream = new AntlrInputStream(input);
+        var lexer = new MashdLexer(inputStream);
+        var tokenStream = new CommonTokenStream(lexer);
+        var parser = new MashdParser(tokenStream);
+        
+        parser.RemoveErrorListeners();
+        parser.AddErrorListener(new ThrowingErrorListener());
+            
+        return parser.program();
+    }
+
     [Fact]
     public void CanParseImportStatement()
     {
         // Arrange
-        string input = "import 'another_file.mashd';";
+        string input = "import \"another_file.mashed\";";
         
         // Act
-        var result = ExpressionParser.Parse(input);
+        var result = ParseProgram(input);
         
         // Assert
         Assert.NotNull(result);
-        Assert.Equal(input, TestHelper.GetTerminalText(result));
+    }
+    
+    [Fact]
+    public void CanParseImportWithAbsolutePath()
+    {
+        // Arrange
+        string input = "import \"/absolute/path/to/file.mashed\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void CanParseImportWithRelativePath()
+    {
+        // Arrange
+        string input = "import \"../relative/path/to/file.mashed\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void CanParseImportWithWindowsPath()
+    {
+        // Arrange
+        string input = "import \"C:\\\\Path\\\\To\\\\File.mashed\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void CanParseImportWithDifferentFileExtensions()
+    {
+            // Arrange
+        string[] inputs = new[]
+        {
+            "import \"file.mashd\";",
+            "import \"file.mshd\";",
+            "import \"file.md\";",
+            "import \"file.txt\";",
+            "import \"file.json\";"
+        };
+        
+        foreach (var input in inputs)
+        {
+            // Act
+            var result = ParseProgram(input);
+            
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+    
+    [Fact]
+    public void CanParseImportWithNoFileExtension()
+    {
+        // Arrange
+        string input = "import \"filename\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void CanParseImportWithSpacesInPath()
+    {
+        // Arrange
+        string input = "import \"path with spaces/file name.mashd\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void CanParseImportWithSpecialCharactersInPath()
+    {
+        // Arrange
+        string input = "import \"path-with_special$chars/file-name_123.mashd\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Theory]
+    [InlineData("import \"valid_file.mashd\";", true)]
+    [InlineData("import valid_file.mashd;", false)]  
+    [InlineData("import \"valid_file.mashd\"", false)]
+    [InlineData("import \"\";", true)] 
+    [InlineData("import;", false)]  
+    public void ValidatesImportStatementSyntax(string input, bool shouldBeValid)
+    {
+        try
+        {
+            // Act
+            var result = ParseProgram(input);
+            
+            // Assert
+            if (shouldBeValid)
+            {
+                Assert.NotNull(result);
+            }
+            else
+            {
+                Assert.True(false, "Parser did not throw an exception for invalid syntax");
+            }
+        }
+        catch (Exception)
+        {
+            Assert.False(shouldBeValid);
+        }
+    }
+    
+    [Fact]
+    public void CanParseMultipleImportStatements()
+    {
+        // Arrange
+        string input = "import \"file1.mashd\";\nimport \"file2.mashd\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void CanParseMixedImportAndStatements()
+    {
+        // Arrange
+        string input = "import \"file1.mashd\";\nInteger x = 42;";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
+    }
+    
+    [Fact]
+    public void CanHandlePathsWithQuoteCharacters()
+    {
+        // Arrange
+        string input = "import \"file_with_\\\"quotes\\\".mashd\";";
+        
+        // Act
+        var result = ParseProgram(input);
+        
+        // Assert
+        Assert.NotNull(result);
     }
 }
