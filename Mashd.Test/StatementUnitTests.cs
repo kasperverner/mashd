f63b64dd2fa1928ae8b86b24fdecde27a17bd0aa
magnuselholm <144026285+magnuselholm@users.noreply.github.com>
2025-05-05 15:57:53 +0200

    Changed structure of tests.

--- Mashd.Test/StatementUnitTests.cs
+++ Mashd.Test/StatementUnitTests.cs
@@ -1,374 +1,81 @@
-using Mashd.Backend;    
+using Mashd.Backend;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Misc;
-using Xunit.Abstractions;
+using Xunit;
 
 namespace TestProject1;
 
 public class StatementUnitTests
 {
-    [Fact]
-    public void CanParseFunctionDeclaration()
+    [Theory]
+    [InlineData("Boolean isEven(Integer x) { return x % 2 == 0; }", typeof(MashdParser.FunctionDefinitionContext))]
+    [InlineData("Integer add(Integer a, Integer b, Integer c) { return a + b + c; }", typeof(MashdParser.FunctionDefinitionContext))]
+    [InlineData("Integer getValue() { return 42; }", typeof(MashdParser.FunctionDefinitionContext))]
+    [InlineData("Integer add(Integer a, Integer b) {}", typeof(MashdParser.FunctionDefinitionContext))]
+    public void CanParseValidDefinitions(string code, Type expectedType)
     {
-        // Arrange
-        var input = TestSnippets.FunctionDeclaration;
-
-        // Act
-        var result = new MashdParser(new CommonTokenStream(new MashdLexer(new AntlrInputStream(input))))
-            .definition(); // Function declarations are part of the 'definition' rule
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.FunctionDefinitionContext>(result);
-    }
-
-    [Fact]
-    public void CanParseIfStatement()
-    {
-        var input = TestSnippets.IfStatement;
-
-        var parser = TestHelper.CreateParser(input);
-        var result = parser.statement();
-
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.IfStatementContext>(result);
-    }
-
-    [Fact]
-    public void CanParseVariableDeclaration()
-    {
-        var input = TestSnippets.VariableDeclaration;
-
-        var parser = TestHelper.CreateParser(input);
-        var result = parser.statement();
-
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.VariableDeclarationContext>(result);
-    }
-
-    [Fact]
-    public void CanParseAssignment()
-    {
-        var input = TestSnippets.Assignment;
-        
-        var parser = TestHelper.CreateParser(input);
-        var result = parser.statement();
-
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.AssignmentContext>(result);
-    }
-
-    [Fact]
-    public void CanParseReturnStatement()
-    {
-        var input = TestSnippets.ReturnStatement;
-        
-        var parser = TestHelper.CreateParser(input);
-        var result = parser.statement();
-
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.ReturnStatementContext>(result);
-    }
-    
-    [Fact]
-    public void ThrowsOnInvalidAssignment()
-    {
-        // Arrange
-        var input = "x + 5 = 10;"; // Invalid assignment
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        
-        // Assert
-        var exception = Assert.Throws<ParseException>(() =>
-            parser.statement());
-
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void ThrowsOnInvalidReturnStatement()
-    {
-        // Arrange
-        var input = "return;";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);;
-        
-        // Assert
-        var exception = Assert.Throws<ParseException>(() =>
-            parser.statement());
-
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void ThrowsOnInvalidIfStatement()
-    {
-        // Arrange
-        var input = "if (true) { return 1; } else;";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        
-        // Assert
-        var exception = Assert.Throws<ParseException>(() =>
-            parser.statement());
-
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void ThrowsOnInvalidFunctionDeclaration()
-    {
-        // Arrange
-        var input = "function Integer add(Integer a, Integer b) { return a + b; } 123;"; 
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        
-        // Assert
-        var exception = Assert.Throws<ParseException>(() =>
-            parser.definition());
-        
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void ThrowsOnInvalidVariableDeclaration()
-    {
-        // Arrange
-        var input = "integer x = ; 10;"; 
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        parser.ErrorHandler = new BailErrorStrategy();
-        
-        // Assert
-        var exception = Assert.Throws<ParseException>(() =>
-            parser.statement());
-        
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void ThrowsOnInvalidFunctionReturnType()
-    {
-        // Arrange
-        var input = "123 add(Integer a, Integer b) { return a + b; }"; 
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        parser.ErrorHandler = new BailErrorStrategy();
-        
-        // Assert
-        var exception = Assert.Throws<ParseException>(() =>
-            parser.definition());
-        
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void CanParseEmptyFunctionBody()
-    {
-        // Arrange
-        var input = "Integer add(Integer a, Integer b) {}"; 
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        parser.ErrorHandler = new BailErrorStrategy();
-        
-        // Assert
-        var result = parser.definition();
-        
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.FunctionDefinitionContext>(result);
-    }
-    
-    [Fact]
-    public void ThrowsOnInvalidNestedIfStatements()
-    {
-        // Arrange
-        var input = "if (true) { if (false) { return 1; } else; }";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        parser.ErrorHandler = new BailErrorStrategy();
-        
-        // Assert
-        var exception = Assert.Throws<ParseException>(() =>
-            parser.statement());
-
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void CanParseVariableDeclarationComplexExpression()
-    {
-        // Arrange
-        var input = "Decimal complex = x + y * (z - 5) / 2;";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        
-        // Assert
-        var result = parser.statement();
-        
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.VariableDeclarationContext>(result);
-    }
-    
-    [Fact]
-    public void CanParseNestedIfStatements()
-    {
-        // Arrange
-        var input = "if (x > 5) { if (y < 10) { return y; } else { return x; } }";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        var result = parser.statement();
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.IfStatementContext>(result);
-    }
-    
-    [Fact]
-    public void CanParseFunctionWithMultipleParameters()
-    {
-        // Arrange
-        var input = "Integer add(Integer a, Integer b, Integer c) { return a + b + c; }";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
+        var parser = TestHelper.CreateParser(code);
         var result = parser.definition();
 
-        // Assert
         Assert.NotNull(result);
-        Assert.IsType<MashdParser.FunctionDefinitionContext>(result);
+        Assert.IsType(expectedType, result);
     }
-    
-    [Fact]
-    public void CanParseFunctionWithNoParameters()
+
+    [Theory]
+    [InlineData("if (true) {return 1;}", typeof(MashdParser.IfStatementContext))]
+    [InlineData("Integer x = 5;", typeof(MashdParser.VariableDeclarationContext))]
+    [InlineData("x = 10;", typeof(MashdParser.AssignmentContext))]
+    [InlineData("return 7;", typeof(MashdParser.ReturnStatementContext))]
+    [InlineData("Decimal complex = x + y * (z - 5) / 2;", typeof(MashdParser.VariableDeclarationContext))]
+    [InlineData("if (x > 5) { if (y < 10) { return y; } else { return x; } }", typeof(MashdParser.IfStatementContext))]
+    [InlineData("if (x > 5) { return x; }", typeof(MashdParser.IfStatementContext))]
+    [InlineData("return x + y;", typeof(MashdParser.ReturnStatementContext))]
+    public void CanParseValidStatements(string code, Type expectedType)
     {
-        // Arrange
-        var input = "Integer getValue() { return 42; }";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        var result = parser.definition();
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.FunctionDefinitionContext>(result);
-    }
-    
-    [Fact]
-    public void CanParseIfStatementWoElse()
-    {
-        // Arrange
-        var input = "if (x > 5) { return x; }";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
+        var parser = TestHelper.CreateParser(code);
         var result = parser.statement();
 
-        // Assert
         Assert.NotNull(result);
-        Assert.IsType<MashdParser.IfStatementContext>(result);
+        Assert.IsType(expectedType, result);
     }
     
-    [Fact]
-    public void EmptyBlockStatement()
+    [Theory]
+    [InlineData("{}", typeof(MashdParser.BlockDefinitionContext))]
+    [InlineData("{ Integer x = 5; x = x + 1; return x; }", typeof(MashdParser.BlockDefinitionContext))]
+    public void CanParseValidBlocks(string code, Type expectedType)
     {
-        // Arrange
-        var input = "{}";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
+        var parser = TestHelper.CreateParser(code);
         var result = parser.block();
 
-        // Assert
         Assert.NotNull(result);
-        Assert.IsType<MashdParser.BlockDefinitionContext>(result);
+        Assert.IsType(expectedType, result);
     }
+
     
-    [Fact]
-    public void MultipleStatementsInBlock()
+    [Theory]
+    [InlineData("x + 5 = 10;")]
+    [InlineData("return;")]
+    [InlineData("if (true) { return 1; } else;")]
+    [InlineData("function Integer add(Integer a, Integer b) { return a + b; } 123;")]
+    [InlineData("integer x = ; 10;")]
+    [InlineData("123 add(Integer a, Integer b) { return a + b; }")]
+    [InlineData("if (true) { if (false) { return 1; } else; }")]
+    public void ThrowsOnInvalidStatements(string input)
     {
-        // Arrange
-        var input = "{ Integer x = 5; x = x + 1; return x; }";
-
-        // Act
         var parser = TestHelper.CreateParser(input);
-        var result = parser.block();
+        parser.ErrorHandler = new BailErrorStrategy();
 
-        // Assert
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.BlockDefinitionContext>(result);
+        Assert.Throws<ParseException>(() => parser.statement());
     }
-    
-    [Fact]
-    public void SingleLineIfStatement()
+
+    [Theory]
+    [InlineData("if (x > 5) { return x; } else { return y;")]
+    [InlineData("if { return x; }")]
+    public void ThrowsParseCanceledExceptionOnCriticalSyntaxErrors(string input)
     {
-        // Arrange
-        var input = "if (x > 5) { return x; }";
-
-        // Act
         var parser = TestHelper.CreateParser(input);
-        var result = parser.statement();
+        parser.ErrorHandler = new BailErrorStrategy();
 
-        // Assert
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.IfStatementContext>(result);
+        Assert.Throws<ParseCanceledException>(() => parser.statement());
     }
-    
-    [Fact]
-    public void ReturnStatementWithExpression()
-    {
-        // Arrange
-        var input = "return x + y;";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        var result = parser.statement();
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.IsType<MashdParser.ReturnStatementContext>(result);
-    }
-
-    [Fact]
-    public void MismatchedBracketStatement()
-    {
-        // Arrange
-        var input = "if (x > 5) { return x; } else { return y;";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        
-        // Assert
-        var exception = Assert.Throws<ParseCanceledException>(() =>
-            parser.statement());
-
-        Assert.NotNull(exception);
-    }
-    
-    [Fact]
-    public void IfStatementNoWoCondition()
-    {
-        // Arrange
-        var input = "if { return x; }";
-
-        // Act
-        var parser = TestHelper.CreateParser(input);
-        
-        // Assert
-        var exception = Assert.Throws<ParseCanceledException>(() =>
-            parser.statement());
-
-        Assert.NotNull(exception);
-    }
-    
-    
 }
--- Mashd.Test/TestSnippets.cs
+++ Mashd.Test/TestSnippets.cs
@@ -2,6 +2,19 @@
 
 public static class TestSnippets
 {
+    public static string Get(string key)
+    {
+        return key switch
+        {
+            "FunctionDeclaration" => FunctionDeclaration,
+            "IfStatement" => IfStatement,
+            "VariableDeclaration" => VariableDeclaration,
+            "Assignment" => Assignment,
+            "ReturnStatement" => ReturnStatement,
+            _ => throw new ArgumentException($"No snippet found for key: {key}")
+        };
+    }
+    
     public const string FunctionDeclaration = "Boolean isEven(int x) { return x % 2 == 0; }";
     public const string IfStatement = "if (true) {return 1;}";
     public const string VariableDeclaration = "Integer x = 5;";
